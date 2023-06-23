7b7335119f57d941442e1f65bdfe0cacb5d6e3c8
syaragamu <takashidoradora@gmail.com>
2023-06-23 23:55:36 +0900

    とくのうとごろふらい

--- game_project/baseball_game/urls.py
+++ game_project/baseball_game/urls.py
@@ -4,6 +4,6 @@ from baseball_game.views import BaseballGameView
 app_name = 'baseball_game'
 
 urlpatterns = [
-    path('', BaseballGameView.as_view(), name='game'),
+    path('', BaseballGameView.as_view(), name='get'),
     path('reset/', BaseballGameView.as_view(), name='game_reset')
 ]
--- game_project/baseball_game/views.py
+++ game_project/baseball_game/views.py
@@ -5,29 +5,46 @@ import random
 class BaseballGameView(View):
     template_name = 'baseball_game/game.html'
 
-    def get(self, request):
-        request.session['strikes'] = 0
-        request.session['outs'] = 0
-        request.session['runners'] = []
-        request.session['point'] = 0
-        request.session['count'] = 0
-        return render(request, self.template_name)
-    
     def reset(self, request):
         request.session['strikes'] = 0
         request.session['outs'] = 0
         request.session['runners'] = []
         request.session['point'] = 0
         request.session['count'] = 0
+        request.session['chance'] = 0
+        request.session['nebari'] = 0
+        request.session['supercount'] = 0
+    
+    def get(self, request):
+        request.session['strikes'] = 0
+        request.session['outs'] = 0
+        request.session['runners'] = []
+        request.session['point'] = 0
+        request.session['count'] = 1
+        request.session['supercount'] = 0
+        request.session['chance'] = 0
+        request.session['nebari'] = 0
+        return render(request, self.template_name)
+    
+        
     
     def post(self, request):
-        pitch = int(request.POST.get('pitch',1))
+        supercount = request.session.get('supercount', 0)
+        chance = request.session.get('chance', 0)
+        nebari = request.session.get('nebari', 0)
+        if supercount >= 1:
+            pass
+        else:
+            chance = int(request.POST.get('chance', 0))
+            nebari = int(request.POST.get('nebari', 0))
+
+        pitch = int(request.POST.get('pitch',0))
         strikes = request.session.get('strikes', 0)
         outs = request.session.get('outs', 0)
         runners = request.session.get('runners', [])
         point = request.session.get('point', 0)
         count = request.session.get('count', 1)
-        ball_list = ["none", "ストレート", "スライダー", "カーブ", "フォーク", "チェンジアップ"]
+        ball_list = ["", "ストレート", "スライダー", "カーブ", "フォーク", "チェンジアップ"]
         def runner_on_first():
             runners.append(1)
 
@@ -146,12 +163,12 @@ class BaseballGameView(View):
         def runner_move_and_point_get(p_ball, chance):
             if p_ball == 1 or p_ball == 2 or p_ball == 4:
                 print("ヒット!!!")
-                if p_ball == 2 and chance == True:
+                if p_ball == 2 and chance == 1 and len(runners) >= 2:
                     niruida()
                 else:
                     itiruida()
             elif p_ball == 5:
-                if chance == True:
+                if chance == 1 and len(runners) >= 2:
                     sannruida()
                 else:
                     niruida()
@@ -160,11 +177,11 @@ class BaseballGameView(View):
 
         def decide_ball():
             t = random.randint(1, 100)
-            if t <= 25:
+            if t <= 24:
                 return 1
-            elif t <= 50:
+            elif t <= 48:
                 return 2
-            elif t <= 75:
+            elif t <= 72:
                 return 4
             elif t <= 90:
                 return 5
@@ -176,11 +193,10 @@ class BaseballGameView(View):
             request.session['outs'] = 0
             request.session['runners'] = []
             request.session['point'] = 0
-            
+            request.session['count'] = 0
 
-        chance = False
 
-        if pitch >= 1 and pitch <= 5:
+        if pitch >= 0 and pitch <= 5:
             p_ball = pitch
         else:
             return render(request, self.template_name, {'error': '正しく入力してください。'})
@@ -189,16 +205,53 @@ class BaseballGameView(View):
         c_ball_name = ball_list[c_ball]
         p_ball_name = ball_list[p_ball]
 
-        if c_ball - p_ball == 0:
+        if p_ball == 0:
+            pass
+        elif c_ball - p_ball == 0:
             runner_move_and_point_get(p_ball, chance)
             point = point_get(point)
             point_write(point)
             strikes = 0
-        else:
+        #ゲッツー
+        elif c_ball == 5 and p_ball == 3:
+            for runner in runners[:]:
+                if runner == 1:
+                    remove_runner_on_first()
+                    outs += 1
+                else:
+                    pass
+            outs += 1
+            strikes = 0
+        #犠牲フライ
+        elif c_ball == 3 and p_ball == 5:
+            for runner in runners[:]:
+                if runner == 3:
+                    if outs <= 10:
+                        outs += 1
+                        point += 1
+                        remove_runner_on_third()
+                    else:
+                        pass
+                else:
+                    pass
+            outs += 1
+            strikes = 0
+        elif p_ball - c_ball != 0:
             print("\n空振り!\n\n")
             strikes += 1
+        else:
+            pass
 
-        if strikes == 3:
+        while strikes == 3:
+            if nebari == 1:
+                if c_ball - p_ball == 1:
+                    strikes -= 1
+                    break
+                elif c_ball == 1 and p_ball == 5:
+                    strikes -= 1
+                    break
+                else:
+                    pass
             print("三振！アウト！\n\n")
             outs += 1
             strikes = 0
@@ -208,20 +261,26 @@ class BaseballGameView(View):
             reset_game()
             return render(request, self.template_name, {'game_status': 'clear', 'point': point})
 
-        if outs == 3:
+        if outs == 10 or outs == 11:
             print("3アウト!この回の攻撃は終了です\n\n\n")
             strikes = 0
             runners = []
             outs = 0
             count = count + 1
 
+        supercount += 1
+        
         request.session['strikes'] = strikes
         request.session['outs'] = outs
         request.session['runners'] = runners
         request.session['point'] = point
         request.session['count'] = count
-
+        request.session['chance'] = chance
+        request.session['nebari'] = nebari
+        request.session['supercount'] = supercount
+        
         return render(request, self.template_name, {'strikes': strikes, 'outs': outs, 'runners': runners, 'point': point,
-                                                    'c_ball_name': c_ball_name,"count" : count,"p_ball_name" :p_ball_name})
+                                                    'c_ball_name': c_ball_name,"count" : count,"p_ball_name" :p_ball_name,
+                                                    'nebari':nebari,'chance':chance,'supercount':supercount})
 
     
